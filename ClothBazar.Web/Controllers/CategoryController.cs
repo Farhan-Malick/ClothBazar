a1e24c927c5d0b7e3b23b95af017b239476524a4
Farhan Malik <farhanbaba1995@gmail.com>
2022-08-31 18:04:42 +0500

    Data posted to db

---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService categoryService = new CategoriesService();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return View();
+        }
+    }
+}
